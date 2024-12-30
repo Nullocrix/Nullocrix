9609b0badef300b743a7e16e2d7d3621c40dcab0
Miguel Karlo Romero <miguel2070romero@gmail.com>
2024-12-30 22:28:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 clear
 echo "It works!"
 
-cat <<'EOF' >change-font.sh
+cat <<'EOF' >change-font
 #!/data/data/com.termux/files/usr/bin/bash
 echo "It works!"
 EOF
-chmod +x change-font.sh
+chmod +x change-font
 mv change-font /data/data/com.termux/files/usr/bin/
 echo "Installation succeeded."
 echo "Runas change-font"
